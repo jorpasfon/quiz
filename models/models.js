15b2305e03e731cfd528645374ac45b14209af5e
jorpasfon <jor.pas.fon@gmail.com>
2015-08-01 14:04:36 +0200

    P2P 7

--- models/models.js
+++ models/models.js
@@ -1,6 +1,6 @@
 var path = require('path');
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// Postgres DATABASE_URL = postgres://user:pass@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
@@ -12,34 +12,38 @@ var port     = (url[5]||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
 
-// Cargar Modelo ORM
+// Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite o Postgres
+// Usar BBDD SQLite o Postgres:
 var sequelize = new Sequelize(DB_name, user, pwd,
-  { dialect:  protocol,
-    protocol: protocol,
-    port:     port,
-    host:     host,
-    storage:  storage,  // solo SQLite (.env)
-    omitNull: true      // solo Postgres
-  }
-);
+                      {
+                        dialect: protocol,
+                        protocol: protocol,
+                        port: port,
+                        host: host,
+                        storage: storage, // solo SQLite (.env)
+                        omitNull: true    // solo Postgres
+                      }
+                    );
 
-// Importar definicion de la tabla Quiz
-var quiz_path = path.join(__dirname,'quiz');
+// Importar la definición de la tabla Quiz
+var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; // exportar tabla Quiz
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
-// sequelize.sync() inicializa tabla de preguntas en DB
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().then(function (count){
-    if(count === 0) {   // la tabla se inicializa solo si está vacía
+  Quiz.count().then(function (count) {
+    if(count === 0) {  // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
-      	            respuesta: 'Roma'
-      	         })
+                    respuesta: 'Roma'
+                  });
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
+                  })
       .then(function(){console.log('Base de datos inicializada')});
     };
   });
